eeccbe29e058adba2d1812db5ae0e84c9235628f
Swati Singh <swati.singh.ug20@nsut.ac.in>
2024-06-11 13:13:01 +0530

    thord commit

--- src/GitNotes.java
+++ src/GitNotes.java
@@ -4,6 +4,14 @@ public class GitNotes {
     //git config --system--->System level configuration
     //git config --global------>User-level-Configuration
     // git config -------->Repo level -configuration
+    //git checkout README.txt ----->this is the command used to revert the changes of a particular file
+    // like here i want ti revert the change of README.txt file of thsi project
+    // git clean -n // would remove unnecessary files
+    //git clean -f // removing the unnecessary files
+    // git init// initialized the empty git repository
+    // git add .// add all the files
+    // git commit -m "first commit"-- till this time working is on local repository
+    // git remote add origin "--------------------" ------->now all the changes are on remote repository
 
 
 }
